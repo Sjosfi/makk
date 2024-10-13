99fa18c71b1b5e8d7cd33f57a8f6252b8a158672
Sjosfi <plavi.barnus@gmail.com>
2024-10-13 21:59:48 +0200

    1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Res, Post, } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Body } from '@nestjs/common';
+import { fizetesDTO } from './fizetesDTO.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
@@ -11,5 +13,39 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  @Get('kitolt')
+  @Render('megerosites')
+  getMegerosites() {
+    let messages = [];
+    return {messages: messages, nev: undefined, szam: undefined}
+  }
+
+  @Post('kitolt')
+  @Render('megerosites')
+  postMegerosites(@Body() paymentDTO: fizetesDTO, @Res() response: any) {
+    let messages = [];
+    if (paymentDTO.nev.length < 1 || !paymentDTO.nev.includes(" ")) {
+      messages.push("rossz a név")
+    }
+    const accountNumberPattern = /^\d{8}-\d{8}(-\d{8})?$/;
+    if (!accountNumberPattern.test(paymentDTO.szam)) {
+      messages.push('Hiba van a bankszámlaszámmal!');
+    }
+    if (paymentDTO.elfogad != "elfogad") {
+      messages.push("Nem fogadta el!")
+    }
+    if(messages.length == 0){
+      return response.redirect("/siker")
+    }else{
+      return {messages: messages, nev:paymentDTO.nev, szam:paymentDTO.szam}
+    }
+  }
+
+  @Get('siker')
+  @Render('siker')
+  getSiker(){
+
   }
 }
---
+++ src/fizetesDTO.dto.ts
@@ -0,0 +1,5 @@
+export class fizetesDTO {
+    nev: string;
+    szam: string;
+    elfogad: string;
+}
